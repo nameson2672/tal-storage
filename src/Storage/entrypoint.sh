eb60b2dce8a9c346c8ea0fdacf39cceb47002030
Nameson Gaudel <namesongaudel.ng@gmail.com>
2025-02-16 18:48:38 -0500

    feat: script to inject appsettings.json from env of render added

---
+++ src/Storage/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+# Ensure /etc/secrets/<filename> exists
+if [ -f "/etc/secrets/appsettings.json" ]; then
+    echo "Copying appsettings.json from /etc/secrets to the app directory..."
+    cp /etc/secrets/appsettings.json /app/appsettings.json
+else
+    echo "Warning: /etc/secrets/appsettings.json not found!"
+fi
+
+# Execute the application
+exec dotnet TalStorage.dll
