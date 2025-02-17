9f1759be7d317e9feac83a153d26ba70891f7d88
Nameson Gaudel <namesongaudel.ng@gmail.com>
2025-02-16 19:38:00 -0500

    fix

--- src/Storage/entrypoint.sh
+++ src/Storage/entrypoint.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
-set -ex 
 
-# Ensure /etc/secrets/<filename> exists
-if [ -f "/etc/secrets/appsettings.json" ]; then
-    echo "Copying appsettings.json from /etc/secrets to the app directory..."
-    cp /etc/secrets/appsettings.json /app/appsettings.json
-else
-    echo "Warning: /etc/secrets/appsettings.json not found!"
-fi
+cp /etc/secrets/appsettings.json /app/
 
 # Execute the application
 exec dotnet /app/TalStorage.dll
